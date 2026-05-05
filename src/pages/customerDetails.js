1afc8f0b3e35e9c27a0e8a583949b87bfef18d00
koil1693 <142288498+koil1693@users.noreply.github.com>
2026-05-05 11:58:48 +0530

    [CR-12] Implement changes

---
+++ src/pages/customerDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomerDetails = () => {
+  return (
+    <div>
+      <h1>Customer Details</h1>
+      {/* Existing design components go here */}
+    </div>
+  );
+};
+
+export default CustomerDetails;
